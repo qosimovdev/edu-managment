10c369297ca4c8cebde039392347ebc20f91e206
ibrohim3 <ibrohim.2128382@gmail.com>
2026-03-05 20:58:01 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,36 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Layout from "./components/layout/Layout";
-import Dashboard from "./pages/Dashboard";
-import Students from "./pages/Students";
-import Teachers from "./pages/Teachers";
-import Courses from "./pages/Courses";
-import Groups from "./pages/Groups";
-import Payments from "./pages/Payments";
-import Attendance from "./pages/Attendance";
-import Login from "./pages/auth/Login";
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Students = lazy(() => import("./pages/Students"));
+const Teachers = lazy(() => import("./pages/Teachers"));
+const Courses = lazy(() => import("./pages/Courses"));
+const Groups = lazy(() => import("./pages/Groups"));
+const Payments = lazy(() => import("./pages/Payments"));
+const Attendance = lazy(() => import("./pages/Attendance"));
+const Login = lazy(() => import("./pages/auth/Login"));
 import "./assets/css/pages.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="students" element={<Students />} />
-        <Route path="teachers" element={<Teachers />} />
-        <Route path="courses" element={<Courses />} />
-        <Route path="groups" element={<Groups />} />
-        <Route path="payments" element={<Payments />} />
-        <Route path="attendance" element={<Attendance />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="students" element={<Students />} />
+          <Route path="teachers" element={<Teachers />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="groups" element={<Groups />} />
+          <Route path="payments" element={<Payments />} />
+          <Route path="attendance" element={<Attendance />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
